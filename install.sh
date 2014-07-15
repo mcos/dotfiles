dfcd8940ad4a6bc195e705437168c160f6c6979d
Mark Costello <mark@markcostello.net>
2014-07-15 17:34:17 -0400

    Added install script and themes

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 # TODO: Clean these up
-ln -s ./zsh/zshrc.symlink ~/.zshrc
+ln -f ./zsh/zshrc.symlink ~/.zshrc
 
-# ATOM
-ln -s ./atom/atom.coffee ~/.atom/atom.coffee
-ln -s ./atom/init.coffee ~/.atom/init.coffee
-ln -s ./atom/config.cson ~/.atom/config.cson
+# Atom
+ln -f ./atom/atom.coffee ~/.atom/atom.coffee
+ln -f ./atom/init.coffee ~/.atom/init.coffee
+ln -f ./atom/config.cson ~/.atom/config.cson
+
+# Powerline Theme
+ln -f ./themes/powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
