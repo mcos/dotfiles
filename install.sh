ec0d75be9d724e3eed4b46ea07c9b9a61f5e77d7
Mark Costello <mark@markcostello.net>
2014-07-15 17:12:47 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# TODO: Clean these up
+ln -s ./zsh/zshrc.symlink ~/.zshrc
+
+# ATOM
+ln -s ./atom/atom.coffee ~/.atom/atom.coffee
+ln -s ./atom/init.coffee ~/.atom/init.coffee
+ln -s ./atom/config.cson ~/.atom/config.cson
