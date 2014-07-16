5bcfba7c0e5cab6013581200b752f28f0487568e
Mark Costello <mark@markcostello.net>
2014-07-16 00:14:09 -0400

    Change history completion

---
+++ zsh/completion.zsh
@@ -0,0 +1,3 @@
+# Search forward in the history for a line beginning with the current line up to the cursor.
+bindkey '\e[A' history-beginning-search-backward
+bindkey '\e[B' history-beginning-search-forward
