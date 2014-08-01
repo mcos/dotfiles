a28f1034fc478abd564c3708bfbbbd32bbe3f251
Mark Costello <mark@markcostello.net>
2014-08-01 15:08:50 -0400

    Updates for aliases and sublime

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias reload!='. ~/.zshrc'
+# alias commitchanges="f() {git whatchanged --since='1 day ago' --pretty=tformat: --numstat | awk \'{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf \"\n Added lines: %s Removed lines : %s Total Lines Changed: %s\n\",add,subs,loc }\'}; f"
