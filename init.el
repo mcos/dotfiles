2af6ac77bba474bd0d9dfa06fd04c45cc0c252ce
Mark Costello <mark@markcostello.net>
2015-03-13 10:04:07 -0400

    Updates for emacs and zsh theming

---
+++ init.el
@@ -0,0 +1,37 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+;;(add-to-list 'package-archives
+;;             '("marmalade" . "https://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; List of packages to use
+(defvar my-packages '(better-defaults
+                      magit
+                      idle-highlight-mode
+                      markdown-mode
+                      projectile
+                      color-theme-sanityinc-tomorrow
+                      autopair
+                      exec-path-from-shell
+                      company
+                      company-go
+                      go-mode
+                      php-mode
+                      ws-butler
+                      flycheck)
+  "A list of packages that should be installed at launch")
+
+;; Make sure the packages are up to date
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Load My Custom Config
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(setq user-specific-config (concat dotfiles-dir "mcos.el"))
+(if (file-exists-p user-specific-config) (load user-specific-config))
+
---
+++ mcos.el
@@ -0,0 +1,111 @@
+;; Set up our .emacs.d as a variable
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+
+;; Need some Common Lisp Here
+(require 'cl)
+
+;; Projectile for Project Stuff
+(require 'projectile)
+(projectile-global-mode)
+
+;; Company Mode - Autocompletion
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Auto Pair everything
+(require 'autopair)
+(autopair-global-mode) ;; enable autopair in all buffers
+
+;; Require Final Newline
+(setq mode-require-final-newline t)
+
+;; Reload All Changes From Disk
+(global-auto-revert-mode t)
+
+;; Turn off bells
+(setq ring-bell-function 'ignore)
+
+;; Color Theme
+(load-theme 'sanityinc-tomorrow-night t)
+
+;; Turn off the silly startup message
+(setq inhibit-startup-message t)
+
+;; Show whitespace by default
+(require 'ws-butler)
+(ws-butler-global-mode)
+
+;; Save and Restore Desktop Sessions
+(desktop-save-mode 1)
+
+;; GRAB THE PATH
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; Turn on the zshell properly
+(setenv "ESHELL" (concat dotfiles-dir "eshell"))
+
+;; Flycheck Syntax Checking
+(add-hook 'after-init-hook 'global-flycheck-mode)
+
+;;;;;;;;;;;;;;;;;
+;; Go Specific ;;
+;;;;;;;;;;;;;;;;;
+(require 'go-mode)
+(require 'company-go)
+;; Custom go-mode hook
+(add-hook 'go-mode-hook (lambda ()
+  "Custom Go Mode Hook"
+  
+  ;; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
+  
+  ;; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  
+  ;; Imenu & Speedbar
+  (setq imenu-generic-expression
+        '(("type" "^type *\\([^ \t\n\r\f]*\\)" 1)
+          ("func" "^func *\\(.*\\) {" 1)))
+  (imenu-add-to-menubar "Index")
+  (set (make-local-variable 'company-backends) '(company-go))))
+
+;;;;;;;;;;;;;;
+;; PHP-Mode ;;
+;;;;;;;;;;;;;;
+(autoload 'php-mode "php-mode" "Major mode for PHP work." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-hook 'php-mode-hook
+          (setq flycheck-phpcs-standard "PSR2"))
+
+;; Load all projects into magit-repo-dirs
+(eval-after-load "projectile" 
+  '(progn (setq magit-repo-dirs (mapcar (lambda (dir)
+                                         (substring dir 0 -1))
+                                       (remove-if-not (lambda (project)
+                                                        (file-directory-p (concat project "/.git/"))) 
+                                                      (projectile-relevant-known-projects))))
+
+         (setq magit-repo-dirs-depth 1)))
+
+;; Line Numbers
+(global-linum-mode t)
+
+;; Column Numbers
+(column-number-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;
+;; New Key Bindings ;;
+;;;;;;;;;;;;;;;;;;;;;;
+
+;; Map C-x C-m to M-x - So we don't need to searching for Alt
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+;; C-w is now delete the current word. So we don't have to go and find BACKSPACE
+(global-set-key "\C-w" 'backward-kill-word)
+
+;; Remap Kill-Region to C-x C-k
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
