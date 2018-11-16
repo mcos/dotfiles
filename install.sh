17fd5e5e21c96c2ccee5e2f3e4ac911add0a48f9
Mark Costello <mark@markcostello.net>
2018-11-16 18:26:02 -0500

    More updates

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# admin privileges are needed for this
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until we have finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Require homebrew to be installed
 command -v brew >/dev/null 2>&1 || { echo >&2 "Homebrew is not installed. Install it from https://brew.sh. Exiting."; exit 1; }
 
@@ -9,19 +15,20 @@ command -v brew >/dev/null 2>&1 || { echo >&2 "Homebrew is not installed. Instal
 # Install using the Brewfile
 brew bundle
 
-# change to zsh after we verify it's installed
-command -v zsh >/dev/null 2>&1 || { echo >&2 "zsh is not installed. Exiting."; exit 1; }
+ZSH="$(which zsh)"
 
-chsh -s zsh
+echo $ZSH >> /etc/shells
+
+chsh -s "$(which zsh)"
 
 # Link up all the required files.
 # zshrc comes first
-ln -sf ./zsh/zshrc.symlink "$HOME/.zshrc"
+ln -sf $PWD/zsh/zshrc.symlink "$HOME/.zshrc"
 # git
-ln -sf ./git/gitconfig.symlink "$HOME/.gitconfig"
-ln -sf ./git/gitignore.symlink "$HOME/.gitignore"
+ln -sf $PWD/git/gitconfig.symlink "$HOME/.gitconfig"
+ln -sf $PWD/git/gitignore.symlink "$HOME/.gitignore"
 # gpg
-ln -sf ./gpg/gpg-agent.conf.symlink "$HOME/.gnupg/gpg-agent.conf"
-ln -sf ./gpg/gpg.conf.symlink "$HOME/.gnupg/gpg.conf"
+ln -sf $PWD/gpg/gpg-agent.conf.symlink "$HOME/.gnupg/gpg-agent.conf"
+ln -sf $PWD/gpg/gpg.conf.symlink "$HOME/.gnupg/gpg.conf"
 # intellij
-ln -sf ./intellij/ideavimrc.symlink "$HOME/.ideavimrc"
+ln -sf $PWD/intellij/ideavimrc.symlink "$HOME/.ideavimrc"
