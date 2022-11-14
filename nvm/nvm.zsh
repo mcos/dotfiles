cafc7df0ef2d3b28a6e09542808dd6a6424ac302
Mark Costello <mark@markcostello.net>
2022-11-14 11:48:19 -0500

    Updates - for performance

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -1,2 +1,9 @@
 export NVM_DIR="$HOME/.nvm"
-source /usr/local/opt/nvm/nvm.sh
+# source /usr/local/opt/nvm/nvm.sh
+
+nvm() {
+  echo "🚨 NVM not loaded! Loading now..."
+  unset -f nvm
+  export NVM_PREFIX=$(brew --prefix nvm)
+  [ -s "$NVM_PREFIX/nvm.sh" ] && . "$NVM_PREFIX/nvm.sh"  nvm "$@"
+}
